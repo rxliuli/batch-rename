086cd6f98d606799f431411220267dcdace8cf17
rxliuli <rxliuli@gmail.com>
2023-05-12 23:35:21 +0800

    chore: ignore css.d.ts

--- src/renderer/src/EditFileName.module.css.d.ts
+++
@@ -1,9 +0,0 @@
-declare const css: {
-    edit: string
-    editor: string
-    nav: string
-    nav: string
-};
-
-export default css;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi9Vc2Vycy9yeGxpdWxpL2NvZGUvd2ViL2JhdGNoLXJlbmFtZS9zcmMvcmVuZGVyZXIvc3JjL0VkaXRGaWxlTmFtZS5tb2R1bGUuY3NzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7SUFBQyxJQUFLO0lBTUwsTUFBTztJQVlQLEdBQUk7SUFBSixHQUFJIiwiZmlsZSI6Ii9Vc2Vycy9yeGxpdWxpL2NvZGUvd2ViL2JhdGNoLXJlbmFtZS9zcmMvcmVuZGVyZXIvc3JjL0VkaXRGaWxlTmFtZS5tb2R1bGUuY3NzLmQudHMifQ==
