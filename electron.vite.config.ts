932df0362b0ba32802ec5a242233f343317dcfe7
rxliuli <rxliuli@gmail.com>
2023-05-13 19:30:31 +0800

    chore: add pwa config

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import { cssdts } from '@liuli-util/vite-plugin-css-dts'
 import { i18nextDtsGen } from '@liuli-util/rollup-plugin-i18next-dts-gen'
 import { plugin, Mode } from 'vite-plugin-markdown'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   main: {
@@ -24,7 +25,48 @@ export default defineConfig({
       i18nextDtsGen({
         dirs: ['src/renderer/src/i18n'],
       }),
-      plugin({ mode: [Mode.HTML, Mode.REACT] }),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          id: 'rxliuli.batch-rename',
+          short_name: ' batch-rename',
+          name: 'Batch File Renaming',
+          icons: [
+            {
+              src: '/icon.png',
+              sizes: '256x256',
+              type: 'image/png',
+            },
+          ],
+          file_handlers: [
+            {
+              action: './',
+              accept: {
+                // ref: https://developer.mozilla.org/zh-CN/docs/Web/Media/Formats/Image_types
+                'image/*': [
+                  '.apng',
+                  '.avif',
+                  '.bmp',
+                  '.gif',
+                  '.ico',
+                  '.cur',
+                  '.jpg',
+                  '.jpeg',
+                  '.jfif',
+                  '.pjpeg',
+                  '.pjp',
+                  '.png',
+                  '.svg',
+                  '.tif',
+                  '.tiff',
+                  '.webp',
+                ],
+              },
+            },
+          ],
+        },
+      }),
+      plugin({ mode: [Mode.HTML] }),
     ],
   },
 })
